6f82934ebaffc463eda43d679f7b1e1a3b4e8756
Wuwei Lan <lan@Wuweis-MacBook-Pro.local>
2016-11-06 23:57:11 -0500

    fix some bugs

--- k-means.py
+++ k-means.py
@@ -21,9 +21,9 @@ def readData(filename):
                 firstLine=False
                 continue
             else:
-                if filename=='wine.csv':
-                    data.append([float(item) for item in row[1:-2]])
-                    label.append(row[-2])
+                if filename=='wine_cleaned.csv':
+                    data.append([float(item) for item in row[0:-1]])
+                    label.append(row[-1])
                 else:
                     data.append([float(item) for item in row[1:-1]])
                     label.append(row[-1])
@@ -35,14 +35,24 @@ def euclideanDistance(x1,x2):
 def SilhouetteCoefficient(dataInstance,dataInstanceLabel):
     count=0
     a=0
-    b=float("inf")
+    b=[]
     s=0
+    count_b=[]
+    for i in range(k):
+        b.append(0)
+        count_b.append(0)
     for i,item in enumerate(data):
         if predictedlabel[i]==dataInstanceLabel:
             a+=euclideanDistance(dataInstance,item)
             count+=1
         else:
-            b=min(b,euclideanDistance(dataInstance,item))
+            b[predictedlabel[i]]+=euclideanDistance(dataInstance,item)
+            count_b[predictedlabel[i]]+=1
+    for i in range(k):
+        if count_b[i]:
+            b[i]=b[i]/count_b[i]
+    b.remove(0)
+    b=min(b)
     if count:
         a=a/count
     s=(b-a)/max(a,b)
@@ -59,6 +69,8 @@ def dataAnalysis():
     SSB_predicted=0
     Silhouette=[]
     M=[]
+    for i in range(len(data[0])):
+        M.append(0)
     for i in range(k):
         actualCentroids.append([0 for item in data[i]])
         countCentroids.append(0)
@@ -66,15 +78,15 @@ def dataAnalysis():
         SSE_actual.append(0)
         SSE_predict.append(0)
         Silhouette.append(0)
-        M.append(0)
     for i,dataInstance in enumerate(data):
         countCentroids_predicted[predictedlabel[i]]+=1
-        centroidIndex=int(actuallabel[i])-1
-        countCentroids[centroidIndex] += 1
-        actualCentroids[centroidIndex] = [sum(x) for x in zip(actualCentroids[centroidIndex], dataInstance)]
+        #centroidIndex=int(actuallabel[i])-1
+        #countCentroids[centroidIndex] += 1
+        #actualCentroids[centroidIndex] = [sum(x) for x in zip(actualCentroids[centroidIndex], dataInstance)]
         M=[sum(x) for x in zip(M,dataInstance)]
         Silhouette[predictedlabel[i]]+=SilhouetteCoefficient(dataInstance,predictedlabel[i])
     M=[item/len(data) for item in M]
+    Silhouette_overall=sum(Silhouette)/len(data)
     for i in range(k):
         if countCentroids[i]:
             actualCentroids[i] = [item / countCentroids[i] for item in actualCentroids[i]]
@@ -82,18 +94,26 @@ def dataAnalysis():
             Silhouette[i]=Silhouette[i]/countCentroids_predicted[i]
     for i,dataInstance in enumerate(data):
         SSE_predict[predictedlabel[i]]+=(euclideanDistance(dataInstance,centroids[predictedlabel[i]]))**2
-        SSE_actual[int(actuallabel[i])-1]+=(euclideanDistance(dataInstance,actualCentroids[int(actuallabel[i])-1]))**2
+        #SSE_actual[int(actuallabel[i])-1]+=(euclideanDistance(dataInstance,actualCentroids[int(actuallabel[i])-1]))**2
     for i in range(k):
-        SSB_actual+=countCentroids[i]*(euclideanDistance(actualCentroids[i],M))**2
+        #SSB_actual+=countCentroids[i]*(euclideanDistance(actualCentroids[i],M))**2
         SSB_predicted+=countCentroids_predicted[i]*(euclideanDistance(centroids[i],M))**2
+    '''
     print 'SSB actual and SSB predicted'
     print SSB_actual,SSB_predicted
     print 'SSE actual and SSE predicted'
     print SSE_actual,SSE_predict
     print sum(SSE_actual),sum(SSE_predict)
+    '''
+    print 'SSB predicted'
+    print SSB_predicted
+    print 'SSE predicted'
+    print SSE_predict, sum(SSE_predict)
     print 'Cluster Silhouette and Overall Silhouette'
-    print Silhouette,sum(Silhouette)
-
+    print Silhouette,Silhouette_overall
+    print 'M'
+    print M
+    #sys.exit()
     c1=[]
     c2=[]
     c3=[]
@@ -112,7 +132,7 @@ def dataAnalysis():
             c3[predictedlabel[i]] += 1
         elif actuallabel[i]=='4':
             c4[predictedlabel[i]]+=1
-        print actuallabel[i], predictedlabel[i]
+        #print actuallabel[i], predictedlabel[i]
     print Counter(actuallabel).keys()
     print Counter(actuallabel).values()
     print c1,c2,c3,c4
@@ -140,9 +160,9 @@ def dataAnalysis():
             Y3.append(data[i][1])
     plt.scatter(X,Y, marker='o', c='b',label='predicted class 1')
     plt.scatter(X1,Y1, marker='x', c='r',label='predicted class 2')
+    #plt.scatter(X2, Y2, marker='o', c='g', label='predicted class 3')
+    #plt.scatter(X3, Y3, marker='x', c='y', label='predicted class 4')
     if inputFile == 'TwoDimHard.csv':
-        plt.scatter(X2,Y2, marker='o', c='g',label='predicted class 3')
-        plt.scatter(X3,Y3, marker='x', c='y',label='predicted class 4')
         plt.legend(loc='lower right', ncol=2, fontsize=10)
     else:
         plt.legend(loc='upper right',ncol=2,fontsize=12)
@@ -185,26 +205,27 @@ def kMeans(k):
         if newcentroids[0][0]<newcentroids[1][0]:
             newcentroids[0],newcentroids[1]=newcentroids[1],newcentroids[0]
     elif inputFile=='TwoDimHard.csv':
-        x_value=[newcentroids[0][0],newcentroids[1][0],newcentroids[2][0],newcentroids[3][0]]
-        y_value=[newcentroids[0][1],newcentroids[1][1],newcentroids[2][1],newcentroids[3][1]]
-        y_value.sort()
-        index=sorted(range(len(y_value)), key=lambda k: y_value[k])
-        temp=[x_value[index[0]],x_value[index[1]],x_value[index[2]],x_value[index[3]]]
-        x_value=temp
-        if x_value[-1]<x_value[-2]:
-            newcentroids[0]=[x_value[-1],y_value[-1]]
-            newcentroids[1]=[x_value[-2],y_value[-2]]
-        else:
-            newcentroids[0] = [x_value[-2], y_value[-2]]
-            newcentroids[1] = [x_value[-1], y_value[-1]]
-        if x_value[-3]<x_value[0]:
-            newcentroids[2]=[x_value[-3],y_value[-3]]
-            newcentroids[3]=[x_value[0],y_value[0]]
-        else:
-            newcentroids[3]=[x_value[-3],y_value[-3]]
-            newcentroids[2]=[x_value[0],y_value[0]]
+        if k==4:
+            x_value=[newcentroids[0][0],newcentroids[1][0],newcentroids[2][0],newcentroids[3][0]]
+            y_value=[newcentroids[0][1],newcentroids[1][1],newcentroids[2][1],newcentroids[3][1]]
+            y_value.sort()
+            index=sorted(range(len(y_value)), key=lambda k: y_value[k])
+            temp=[x_value[index[0]],x_value[index[1]],x_value[index[2]],x_value[index[3]]]
+            x_value=temp
+            if x_value[-1]<x_value[-2]:
+                newcentroids[0]=[x_value[-1],y_value[-1]]
+                newcentroids[1]=[x_value[-2],y_value[-2]]
+            else:
+                newcentroids[0] = [x_value[-2], y_value[-2]]
+                newcentroids[1] = [x_value[-1], y_value[-1]]
+            if x_value[-3]<x_value[0]:
+                newcentroids[2]=[x_value[-3],y_value[-3]]
+                newcentroids[3]=[x_value[0],y_value[0]]
+            else:
+                newcentroids[3]=[x_value[-3],y_value[-3]]
+                newcentroids[2]=[x_value[0],y_value[0]]
     print newcentroids
-    newcentroids=[[0,1],[1,1],[0,0],[1,0]]
+    #newcentroids=[[0,1],[1,1],[0,0],[1,0]]
     while 1:
         predictedlabel = []
         lastCentroids = newcentroids[:]
@@ -226,25 +247,26 @@ def kMeans(k):
         print lastCentroids
         print  newcentroids
         print '------------------------'
-        if newcentroids[0]==lastCentroids[0]:
+        if newcentroids==lastCentroids:
             break
     return newcentroids,predictedlabel
 
 if __name__=='__main__':
-    #inputFile='TwoDimEasy.csv'
-    inputFile='TwoDimHard.csv'
-    #inputFile='wine.csv'
+    inputFile='TwoDimEasy.csv'
+    #inputFile='TwoDimHard.csv'
+    #inputFile='wine_cleaned.csv'
     title=[]
     data,actuallabel=readData(inputFile)
-    k=4
+    k=2
     centroids,predictedlabel=kMeans(k)
-    '''
+    #predictedlabel=[int(i)-1 for i in actuallabel]
+
     with open('output.csv','a+') as f:
         pointer=csv.writer(f)
         pointer.writerow(title)
         for i,label in enumerate(predictedlabel):
             pointer.writerow([i+1,label])
-    '''
+
     print centroids
     #kmeans = KMeans(n_clusters=k, random_state=0).fit(data)
     #print kmeans.cluster_centers_
